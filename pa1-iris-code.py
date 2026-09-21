e07494ddfef2510eb251e05c69d67c8dfee2cfd3
quesoazucarado-ctrl <quesoazucarado@gmail.com>
2026-09-21 15:48:28 -0500

    Delete pa1-iris-code.py

--- pa1-iris-code.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-url = "https://raw.githubusercontent.com/quesoazucarado-ctrl/pa1-machinelearning/refs/heads/main/iris.csv"
-
-df = pd.read_csv(url, header = None)
-
-df.head()
